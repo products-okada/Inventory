0c8801a9d3e9805c33933b515b5fcd98379bb2d0
products_okada <ryohei.okada@hakuhodo.co.jp>
2021-11-10 17:48:34 +0900

    商品データviewの表示

--- app/Http/Controllers/ProductDataController.php
+++ app/Http/Controllers/ProductDataController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductDataController extends Controller
 {
-    //
+    public function signup(){
+        return view('productSignup');
+    }
+
+    public function list(){
+        return view('productList');
+    }
+
+    public function edit(){
+        return view('productEdit');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HelloLaravelController;
 Route::get('/HelloLaravel',[HelloLaravelController::class, 'index']);
+
+use App\Http\Controllers\ProductDataController;
+Route::get('/productSingup',[ProductDataController::class, 'signup']);
+Route::get('/productList',[ProductDataController::class, 'list']);
+Route::get('/productEdit',[ProductDataController::class, 'edit']);
+
+
 Route::get('/view', function () {
     return view('welcome');
 });
